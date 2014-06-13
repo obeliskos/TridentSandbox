6b3565ca888ff547c5d56edd17e926c8bfd333e0
obeliskos <admin@obeliskos.com>
2014-06-13 06:39:30 -0400

    1.87 Changes
    - CodeMirror Search enabled
    - Added Babylon.js, moment.js, and jsoneditor.js libraries with samples
    - API_Restore_TridentDB() now refreshes the trident save slot list
    - Added 'Days Since/Days Until' sample app and Streaming Radio for x86
    (wmp embed)
    - Local FS now shows Samples Browser button for accessing those samples
    online
    - TridentList added functions for setting list item as html
    - API_ImportScriptUnit optional callback param to call after script is
    ready

--- scripts/tridentlist.js
+++ scripts/tridentlist.js
@@ -11,118 +11,152 @@ function TridentList(listId, userCallback) {
     	clickCallback : userCallback,
 		bright: false
     };
-    
-	this.makeBright = function() {
-		this.listSettings.bright = true;
-		
-		$("#" + this.listSettings.listElementId).attr("class", "tlist-bright");
-		$("#" + this.listSettings.listElementId).find("div.tlistitem").attr("class", "tlistitem-bright");
-		$("#" + this.listSettings.listElementId).find("div.tlistitem-selected").attr("class", "tlistitem-selected-bright");
-		$("#" + this.listSettings.listElementId).find("span.tlistitem-caption").attr("class", "tlistitem-caption-bright");
-		$("#" + this.listSettings.listElementId).find("span.tlistitem-description").attr("class", "tlistitem-description-bright");
-	}
+}
+
+TridentList.prototype.makeBright = function() {
+	this.listSettings.bright = true;
 	
-	this.setCaptionColor = function(colorCode) {
-		$("#" + this.listSettings.listElementId).find("span.tlistitem-caption").css("color", colorCode);
-		$("#" + this.listSettings.listElementId).find("span.tlistitem-caption-bright").css("color", colorCode);
-	}
+	$("#" + this.listSettings.listElementId).attr("class", "tlist-bright");
+	$("#" + this.listSettings.listElementId).find("div.tlistitem").attr("class", "tlistitem-bright");
+	$("#" + this.listSettings.listElementId).find("div.tlistitem-selected").attr("class", "tlistitem-selected-bright");
+	$("#" + this.listSettings.listElementId).find("span.tlistitem-caption").attr("class", "tlistitem-caption-bright");
+	$("#" + this.listSettings.listElementId).find("span.tlistitem-description").attr("class", "tlistitem-description-bright");
+}
+
+TridentList.prototype.setCaptionColor = function(colorCode) {
+	$("#" + this.listSettings.listElementId).find("span.tlistitem-caption").css("color", colorCode);
+	$("#" + this.listSettings.listElementId).find("span.tlistitem-caption-bright").css("color", colorCode);
+}
+
+TridentList.prototype.clearList = function() {
+	$("#" + this.listSettings.listElementId).empty(); 
+}
+
+TridentList.prototype.addListItem = function(id, caption, description, descIsHtml) {
+	var captionSpan = document.createElement("span");
 	
-	this.clearList = function() {
-		$("#" + this.listSettings.listElementId).empty(); 
-	};
+	if (this.listSettings.bright) captionSpan.className = "tlistitem-caption-bright";
+	else captionSpan.className = "tlistitem-caption";
+	
+	$(captionSpan).text(caption);
 
-	this.addListItem = function(id, caption, description) {
-		var captionSpan = document.createElement("span");
-		
-		if (this.listSettings.bright) captionSpan.className = "tlistitem-caption-bright";
-    	else captionSpan.className = "tlistitem-caption";
-		
-    	$(captionSpan).text(caption);
-    
-    	var descSpan = document.createElement("span");
+	var descSpan = document.createElement("span");
 
-		if (this.listSettings.bright) descSpan.className = "tlistitem-description-bright";
-    	else descSpan.className = "tlistitem-description";
-		
-    	if (description) $(descSpan).text(description);
-    
-    	var listitemDiv = document.createElement("div");
+	if (this.listSettings.bright) descSpan.className = "tlistitem-description-bright";
+	else descSpan.className = "tlistitem-description";
+	
+	if (typeof(description) != "undefined") {
+		if ((typeof(descIsHtml) != "undefined") && descIsHtml) $(descSpan).html(description);
+		else $(descSpan).text(description);
+	}
 
-		if (this.listSettings.bright) listitemDiv.className = "tlistitem-bright";
-    	else listitemDiv.className = "tlistitem";
-		
-		$(listitemDiv).attr("aria-describedby", id);
-    	listitemDiv.appendChild(captionSpan);
-    	listitemDiv.appendChild(document.createElement("br"));
-    	listitemDiv.appendChild(descSpan);
-		listitemDiv.onclick = (function(id, settings) {
-			return function() { 
-				if (settings.bright) {
-					$("#" + settings.listElementId).find("div.tlistitem-selected-bright").attr("class", "tlistitem-bright");
-					this.className = "tlistitem-selected-bright";
-				}
-				else {
-					$("#" + settings.listElementId).find("div.tlistitem-selected").attr("class", "tlistitem");
-					this.className = "tlistitem-selected";
-				}
+	var listitemDiv = document.createElement("div");
 
-                settings.selectedId = id;
-                if (settings.clickCallback) settings.clickCallback(settings.listElementId, id);
+	if (this.listSettings.bright) listitemDiv.className = "tlistitem-bright";
+	else listitemDiv.className = "tlistitem";
+	
+	$(listitemDiv).attr("data-item-id", id);
+	listitemDiv.appendChild(captionSpan);
+	listitemDiv.appendChild(document.createElement("br"));
+	listitemDiv.appendChild(descSpan);
+	listitemDiv.onclick = (function(id, settings) {
+		return function() { 
+			if (settings.bright) {
+				$("#" + settings.listElementId).find("div.tlistitem-selected-bright").attr("class", "tlistitem-bright");
+				this.className = "tlistitem-selected-bright";
 			}
-		})(id, this.listSettings);
+			else {
+				$("#" + settings.listElementId).find("div.tlistitem-selected").attr("class", "tlistitem");
+				this.className = "tlistitem-selected";
+			}
 
-		var elem = document.createElement("li");
-		$(elem).attr("aria-describedby", id);
-    	elem.appendChild(listitemDiv);
-    
-    	var dlv = document.getElementById(this.listSettings.listElementId);
-    	dlv.appendChild(elem);
-	};
-    
-    // deletes the specified item id 
-    // if no id is passed into this function, it will delete the selected item
-    this.deleteListItem = function(id) {
-    	if (id == null) id = this.listSettings.selectedId;
-        
-        // find the LI within this UL that has the id specified in param
-        $("#" + this.listSettings.listElementId).find("li[aria-describedby="+id+"]").remove();
+			settings.selectedId = id;
+			if (settings.clickCallback) settings.clickCallback(settings.listElementId, id);
+		}
+	})(id, this.listSettings);
+
+	var elem = document.createElement("li");
+	$(elem).attr("data-item-id", id);
+	elem.appendChild(listitemDiv);
+
+	var dlv = document.getElementById(this.listSettings.listElementId);
+	dlv.appendChild(elem);
+}
+
+TridentList.prototype.addListItemRaw = function(id, itemHtml) {
+	var listitemDiv = document.createElement("div");
+
+	if (this.listSettings.bright) listitemDiv.className = "tlistitem-bright";
+	else listitemDiv.className = "tlistitem";
+	
+	$(listitemDiv).attr("data-item-id", id);
+	$(listitemDiv).html(itemHtml);
+	listitemDiv.onclick = (function(id, settings) {
+		return function() { 
+			if (settings.bright) {
+				$("#" + settings.listElementId).find("div.tlistitem-selected-bright").attr("class", "tlistitem-bright");
+				this.className = "tlistitem-selected-bright";
+			}
+			else {
+				$("#" + settings.listElementId).find("div.tlistitem-selected").attr("class", "tlistitem");
+				this.className = "tlistitem-selected";
+			}
+
+			settings.selectedId = id;
+			if (settings.clickCallback) settings.clickCallback(settings.listElementId, id);
+		}
+	})(id, this.listSettings);
+
+	var elem = document.createElement("li");
+	$(elem).attr("data-item-id", id);
+	elem.appendChild(listitemDiv);
+
+	var dlv = document.getElementById(this.listSettings.listElementId);
+	dlv.appendChild(elem);
+}
+
+// deletes the specified item id 
+// if no id is passed into this function, it will delete the selected item
+TridentList.prototype.deleteListItem = function(id) {
+	if (id == null) id = this.listSettings.selectedId;
+	
+	// find the LI within this UL that has the id specified in param
+	$("#" + this.listSettings.listElementId).find("li[data-item-id="+id+"]").remove();
+	
+	this.listSettings.selectedId = null;
+}
+
+TridentList.prototype.updateListItem = function(id, caption, description) {
+	var item = $("#" + this.listSettings.listElementId).find("li[data-item-id="+id+"]");
+	
+	if (this.listSettings.bright) {
+		$(item).find("span.tlistitem-caption-bright").text(caption);
+		$(item).find("span.tlistitem-description-bright").text(description);
+	}
+	else {
+		$(item).find("span.tlistitem-caption").text(caption);
+		$(item).find("span.tlistitem-description").text(description);
+	}
+}
+
+TridentList.prototype.setSelection = function(id) {
+	// clear previous selection
+	$("#" + this.listSettings.listElementId).find("div.tlistitem-selected").attr("class", "tlistitem");
+	$("#" + this.listSettings.listElementId).find("div.tlistitem-selected-bright").attr("class", "tlistitem-bright");
+	
+	// find the div within this list that has the id specified in param
+	var item = $("#" + this.listSettings.listElementId).find("div[data-item-id="+id+"]");
+
+	// if it found the item with that id, select it
+	if (item && item.length > 0) {
+		// apply 'selected' css class
+		if (this.listSettings.bright) $(item).addClass("tlistitem-selected-bright");
+		else $(item).addClass("tlistitem-selected");
 		
+		this.listSettings.selectedId = id;
+	}
+	else {
+		// id was not found, we un-selected any previous selection so clear selectedId
 		this.listSettings.selectedId = null;
-    };
-    
-    this.updateListItem = function(id, caption, description) {
-        var item = $("#" + this.listSettings.listElementId).find("li[aria-describedby="+id+"]");
-    	
-		if (this.listSettings.bright) {
-			$(item).find("span.tlistitem-caption-bright").text(caption);
-			$(item).find("span.tlistitem-description-bright").text(description);
-		}
-		else {
-			$(item).find("span.tlistitem-caption").text(caption);
-			$(item).find("span.tlistitem-description").text(description);
-		}
-    }
-    
-    // pass in the item id you want 'selected'
-    this.setSelection = function(id) {
-    	// clear previous selection
-		$("#" + this.listSettings.listElementId).find("div.tlistitem-selected").attr("class", "tlistitem");
-		$("#" + this.listSettings.listElementId).find("div.tlistitem-selected-bright").attr("class", "tlistitem-bright");
-        
-        // find the div within this list that has the id specified in param
-        var item = $("#" + this.listSettings.listElementId).find("div[aria-describedby="+id+"]");
-
-		// if it found the item with that id, select it
-		if (item && item.length > 0) {
-        	// apply 'selected' css class
-			if (this.listSettings.bright) $(item).addClass("tlistitem-selected-bright");
-			else $(item).addClass("tlistitem-selected");
-            
-            this.listSettings.selectedId = id;
-        }
-        else {
-        	// id was not found, we un-selected any previous selection so clear selectedId
-            this.listSettings.selectedId = null;
-        }
-     }
+	}
 }
