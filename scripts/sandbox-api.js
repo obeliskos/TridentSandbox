c7344fce15e49983a47372524b3f97e3a6aa7391
obeliskos <admin@obeliskos.com>
2016-12-21 06:35:12 -0500

    Updated loki and fixed daylight graph not showing in samples
    - loki had new $between operator added and fixed persisting ChangesAPI
    across saves
    - fixed daylight graph not appearing in samples

--- scripts/sandbox-api.js
+++ scripts/sandbox-api.js
@@ -2761,7 +2761,7 @@ var sandbox = {
     dashboard: sandboxDashboard,
     editorModeEnum: Object.freeze({ "Markup": 1, "Split": 2, "Script": 3 }),
     volatile: {
-        version: "2.2.7",
+        version: "2.2.8",
         env: '',    // page should set this in document.ready to 'WJS IDE', 'IDE', 'SBL', 'SBL WJS', or 'SA'
         online: function () { return navigator.onLine; },
         vars: null,
